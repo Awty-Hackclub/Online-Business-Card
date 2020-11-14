6881f5abfc91503d00d099a837a4645bdcf973f7
Maxime Morize <morizem@awty.org>
2020-11-14 16:31:11 -0600

    fuck you terence

---
+++ app/frontend/src/CardComponents/Card.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div class="card">
+      <h3 class="companyName">Company Name</h3>
+      <div class="column">
+        <div class="URLS">
+          <ul>
+            <li>Twitter</li>
+            <li>Instagram</li>
+            <li>Youtube</li>
+            <li>LinkedIN</li>
+          </ul>
+        </div>
+        <div class="description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
