06a696701b58207505002079253234d58fbbb6c5
Maxime Morize <maxime.morize@outlook.com>
2020-11-14 11:55:26 -0600

    format changes

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
-import Input from './Components/Input.js'
+import Input from './Components/Input'
+import Title from './Components/Title'
 
 function App() {
   return (
     <div>
+      <Title />
       <Input/>
     </div>
   );
