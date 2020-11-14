9732b63be98bb233ea08197e74e3d6d986a175b7
Tarek Ali <tarekali15@outlook.com>
2020-11-14 08:04:44 -0600

    semi-finalized db schema

--- app/backend/db.sql
+++ app/backend/db.sql
@@ -1,10 +1,9 @@
 CREATE TABLE Card (
-    id VARCHAR(20),
+    id VARCHAR(36) PRIMARY KEY,
     title VARCHAR(50),
     youtube VARCHAR(2000),
     instagram VARCHAR(2000),
     twitter VARCHAR(2000),
     facebook VARCHAR(2000),
-    description TEXT,
-    PRIMARY KEY(id)
+    description TEXT
 );
