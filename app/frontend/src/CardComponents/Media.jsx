b50d8b481e4f860e7bb21ccd943db01cedb96266
gallo-json <gallojsantiago@gmail.com>
2020-11-14 17:11:18 -0600

    icons

--- app/frontend/src/CardComponents/Media.jsx
+++ app/frontend/src/CardComponents/Media.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
 import youtubeIcon from '../img/youtube.svg'
+import instagramIcon from '../img/instagram.svg'
+import twitterIcon from '../img/twitter.svg'
 
 export default function Media(props){
-
-    return(
-        <div>
-            <div className = "media">
-                <label>{props.platform}</label>
-                <label>{props.link}</label>
-                <img src={youtubeIcon} alt="youtube"/>
+    if (props.link === 'http://youtube.com') {
+        return(
+            <div>
+                <div className = "media">
+                    <img src={youtubeIcon} alt="youtube"/>
+                    <label>{props.link}</label>
+                </div>
             </div>
-        </div>
-    )
+        )
+    } else if (props.link === 'http://instagram.com') {
+        return(
+            <div>
+                <div className = "media">
+                    <img src={instagramIcon} alt="youtube"/>
+                    <label>{props.link}</label>
+                </div>
+            </div>
+        )
+    } else if (props.link === 'http://twitter.com') {
+        return(
+            <div>
+                <div className = "media">
+                    <img src={twitterIcon} alt="youtube"/>
+                    <label>{props.link}</label>
+                </div>
+            </div>
+        )
+    } else {
+        return(
+            <div>
+                <div className = "media">
+                    <label>{props.link}</label>
+                </div>
+            </div>
+        )
+    }
 }
