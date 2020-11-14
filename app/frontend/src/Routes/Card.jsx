e02b1179dd0d7861bfbf43cf42d516b3aa87e1f2
Tarek Ali <tarekali15@outlook.com>
2020-11-14 15:34:45 -0600

    basic routes

--- app/frontend/src/Routes/Card.jsx
+++ app/frontend/src/Routes/Card.jsx
@@ -5,5 +5,5 @@ export default function Card() {
         <div>
             <Title content="example" />
         </div>
-    )
+    );
 }
--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Card from "./Routes/Card";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <App />
+        </Route>
+        <Route exact path="/card">
+          <Card />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
