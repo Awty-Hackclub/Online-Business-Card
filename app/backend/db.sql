4641cccd5946e84abfdd2909b73fde62e976ed1b
Tarek Ali <tarekali15@outlook.com>
2020-11-14 07:59:46 -0600

    finished MVP schema for Card; def gonna be iterated on lads

---
+++ app/backend/db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Card (
+    id VARCHAR(20),
+    title VARCHAR(50),
+    youtube VARCHAR(2000),
+    instagram VARCHAR(2000),
+    twitter VARCHAR(2000),
+    facebook VARCHAR(2000),
+    description TEXT,
+    PRIMARY KEY(id)
+);
