59b63253c219f60f79ac8ae5481cbfd4daa506a6
Maxime Morize <maxime.morize@outlook.com>
2020-11-14 12:15:26 -0600

    added footer component

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Input from './Components/Input'
 import Title from './Components/Title'
+import Footer from './Components/Footer'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Input platform="Instagram"/>
       <Input platform="Twitter"/>
       <Input platform="FaceBook"/>
+      <Footer />
     </div>
   );
 }
