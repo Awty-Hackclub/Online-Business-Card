716bd0cc48ffbcd29e230f84f3ee862f03917e1e
Maxime Morize <morizem@awty.org>
2020-11-14 20:11:58 -0600

    centered some shit

--- app/frontend/src/Routes/Card.jsx
+++ app/frontend/src/Routes/Card.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import '../App.css'
-import Title from '../Components/Title'
-import Media from '../CardComponents/Media'
+import React from "react";
+import "../App.css";
+import Title from "../Components/Title";
+import Media from "../CardComponents/Media";
 
 export default function Card(props) {
-    return (
-        <div>
-            <div className = "card">
-                <h1 className = "name">{props.content}</h1>
-                <div className = "medias">
-                    <Media link = "http://youtube.com"/>
-                    <Media link = "http://instagram.com"/>
-                    <Media link = "http://twitter.com"/>
-                    <Media link = "http://facebook.com"/>
-                </div>
-                <div className = "paragraph">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-                </div>
-            </div>
+  return (
+    <div className="card-container">
+      <div className="card">
+        <h1 className="name">{props.content}</h1>
+        <div className="medias">
+          <Media link="http://youtube.com" />
+          <Media link="http://instagram.com" />
+          <Media link="http://twitter.com" />
+          <Media link="http://facebook.com" />
         </div>
-    );
+        <div className="paragraph">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
