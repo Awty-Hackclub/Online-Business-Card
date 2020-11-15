83682f448c8af215f4519641d693a81e8410788d
Tarek Ali <tarekali15@outlook.com>
2020-11-15 08:46:39 -0600

    Create done

--- app/frontend/src/Components/utils/AxiosUtils.js
+++ app/frontend/src/Components/utils/AxiosUtils.js
@@ -1,13 +1,23 @@
 import axios from 'axios';
 
-const Create = () => {
-
+const Create = (object) => {
+    axios.request({
+        method: 'POST',
+        url: 'https://localhost:5000/create',
+        data: {
+            title: object.title,
+            youtube: object.youtube,
+            twitter: object.twitter,
+            facebook: object.facebook,
+            description: object.description
+        },
+    })
 }
 
 const CardJson = (id) => {
     axios.get(f`/localhost:5000/card?iq=${id}`)
         .then(function (response) {
-            console.log(response)
+
         })
 }
 
